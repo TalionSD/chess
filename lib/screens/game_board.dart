27411f980202e55119f93da4b737c9e2e0d2b61c
Saeed Dayani <101660980+TalionSD@users.noreply.github.com>
2023-08-25 19:20:14 +0330

    placement of soldiers on the board

--- lib/screens/game_board.dart
+++ lib/screens/game_board.dart
@@ -12,12 +12,50 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  //create a piece
-  ChessPiece myPawn = ChessPiece(
-    type: ChessPieceType.pwan,
-    isWhite: true,
-    imagePath: 'assets/images/pwan.png',
-  );
+  /// A 2-dimensional list representing the chessboard,
+  /// with each position possibly containing a chess piece
+  late List<List<ChessPiece?>> board;
+
+  @override
+  void initState() {
+    _initializedBoard();
+    super.initState();
+  }
+
+  // INITIALIZE BOARD
+  void _initializedBoard() {
+    // initialize the board with nulls, meaning no pieces in those positions
+    List<List<ChessPiece?>> newBoard = List.generate(
+      8,
+      (index) => List.generate(8, (index) => null),
+    );
+
+    // Place pawns
+    for (int i = 0; i < 8; i++) {
+      newBoard[1][i] = ChessPiece(
+        type: ChessPieceType.pwan,
+        isWhite: false,
+        imagePath: 'assets/images/pwan.png',
+      );
+      newBoard[6][i] = ChessPiece(
+        type: ChessPieceType.pwan,
+        isWhite: true,
+        imagePath: 'assets/images/pwan.png',
+      );
+    }
+    // Place rooks
+
+    // Place knight
+
+    // Place bishope
+
+    // Place quuen
+
+    // Place king
+
+    board = newBoard;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +67,19 @@ class _GameBoardState extends State<GameBoard> {
           crossAxisCount: 8,
         ),
         itemBuilder: (context, index) {
+          // get the row and col position of this square
+          int row = index ~/ 8;
+          int col = index % 8;
+
+          //create a piece
+          ChessPiece myPawn = ChessPiece(
+            type: ChessPieceType.pwan,
+            isWhite: true,
+            imagePath: 'assets/images/pwan.png',
+          );
           return Square(
             isWhite: isWhite(index),
-            piece: myPawn,
+            piece: board[row][col],
           );
         },
       ),
