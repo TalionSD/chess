f85e4f7ab1f851d871ed929b91ee4ae67a6dc6d9
Saeed Dayani <101660980+TalionSD@users.noreply.github.com>
2023-08-27 01:12:48 +0330

    Placing all the game pieces on the board and creating a method for choosing the pieces

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -5,18 +5,42 @@ import 'package:flutter/material.dart';
 class Square extends StatelessWidget {
   final bool isWhite;
   final ChessPiece? piece;
-  const Square({super.key, required this.isWhite, required this.piece});
+  final bool isselected;
+  final void Function()? onTab;
+
+  const Square({
+    super.key,
+    required this.isWhite,
+    required this.piece,
+    required this.isselected,
+    this.onTab,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: isWhite ? forgroundColor : backgroundColor,
-      child: piece != null
-          ? Image.asset(
-              piece!.imagePath,
-              color: piece!.isWhite ? Colors.white : Colors.black,
-            )
-          : null,
+    Color? squareColor;
+
+    // id selected , square is green
+    if (isselected) {
+      squareColor = Colors.green;
+    }
+
+    // otherwise , it's white or black
+    else {
+      squareColor = isWhite ? forgroundColor : backgroundColor;
+    }
+
+    return GestureDetector(
+      onTap: () => onTab,
+      child: Container(
+        color: squareColor,
+        child: piece != null
+            ? Image.asset(
+                piece!.imagePath,
+                color: piece!.isWhite ? Colors.white : Colors.black,
+              )
+            : null,
+      ),
     );
   }
 }
--- lib/screens/game_board.dart
+++ lib/screens/game_board.dart
@@ -16,6 +16,18 @@ class _GameBoardState extends State<GameBoard> {
   /// with each position possibly containing a chess piece
   late List<List<ChessPiece?>> board;
 
+  /// The currently selected piece on the chess board
+  /// If no piece is selected , this is null
+  ChessPiece? selectedPiece;
+
+  /// The row index of the selected piece
+  /// Default value is -1 indicated no piece is currenly selected
+  int selectedRow = -1;
+
+  /// The col index of the selected piece
+  /// Default value is -1 indicated no piece is currenly selected
+  int selectedCol = -1;
+
   @override
   void initState() {
     _initializedBoard();
@@ -44,16 +56,107 @@ class _GameBoardState extends State<GameBoard> {
       );
     }
     // Place rooks
-
+    newBoard[0][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'assets/images/rook.png',
+    );
+    newBoard[0][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'assets/images/rook.png',
+    );
+    newBoard[7][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'assets/images/rook.png',
+    );
+    newBoard[7][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'assets/images/rook.png',
+    );
     // Place knight
+    newBoard[0][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'assets/images/knight.png',
+    );
+    newBoard[0][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'assets/images/knight.png',
+    );
+    newBoard[7][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'assets/images/knight.png',
+    );
+    newBoard[7][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'assets/images/knight.png',
+    );
 
     // Place bishope
+    newBoard[0][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'assets/images/bishop.png',
+    );
+    newBoard[0][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'assets/images/bishop.png',
+    );
+    newBoard[7][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'assets/images/bishop.png',
+    );
+    newBoard[7][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'assets/images/bishop.png',
+    );
 
     // Place quuen
+    newBoard[0][3] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: false,
+      imagePath: 'assets/images/queen.png',
+    );
+    newBoard[7][4] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: true,
+      imagePath: 'assets/images/queen.png',
+    );
 
     // Place king
+    newBoard[0][4] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: false,
+      imagePath: 'assets/images/king.png',
+    );
+    newBoard[7][3] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: true,
+      imagePath: 'assets/images/king.png',
+    );
 
     board = newBoard;
+  }
+
+  //USER SELECTED A PIECE
+  void pieceSelected(int row, int col) {
+    setState(() {
+      //selected a piece if there is a piece in that position
+      if (board[row][col] != null) {
+        selectedPiece = board[row][col];
+        selectedRow = row;
+        selectedCol = col;
+      }
+    });
   }
 
   @override
@@ -71,15 +174,13 @@ class _GameBoardState extends State<GameBoard> {
           int row = index ~/ 8;
           int col = index % 8;
 
-          //create a piece
-          ChessPiece myPawn = ChessPiece(
-            type: ChessPieceType.pwan,
-            isWhite: true,
-            imagePath: 'assets/images/pwan.png',
-          );
+          // check if this square is seleted
+          bool isSelected = selectedRow == row && selectedCol == col;
           return Square(
             isWhite: isWhite(index),
             piece: board[row][col],
+            isselected: isSelected,
+            onTab: () => pieceSelected(row, col),
           );
         },
       ),
